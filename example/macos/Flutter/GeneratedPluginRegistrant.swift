15a7da7ca5fbbd3aca108fdc2bb26ae10eee5d5f
Loïc de Porcaro <deporcaro.loic@gmail.com>
2023-03-13 19:52:57 +0100

    chore: remove unused dependencies
    remove :
    - `url_launcher`
    - `uni_links`
    - `cupertino_icons`

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,9 @@ import Foundation
 import flutter_web_auth
 import path_provider_foundation
 import shared_preferences_foundation
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterWebAuthPlugin.register(with: registry.registrar(forPlugin: "FlutterWebAuthPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
