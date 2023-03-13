190655ffec634f507fdedacfe3ce8c256b590fbb
Loïc de Porcaro <deporcaro.loic@gmail.com>
2023-03-13 19:36:38 +0100

    chore: upgrade dependencies

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import flutter_web_auth
-import path_provider_macos
+import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 
