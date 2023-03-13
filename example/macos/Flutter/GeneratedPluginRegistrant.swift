29a998114b54cbe4fd5c483399dc0a84cbec9eb4
Loïc de Porcaro <nenuphar12@protonmail.com>
2023-03-13 20:12:29 +0100

    chore(example): remove unused dependencies in example
    remove :
    - `html`
    - `path_provider`
    - `cupertino_icons`
    - `flutter_driver`

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_web_auth
-import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterWebAuthPlugin.register(with: registry.registrar(forPlugin: "FlutterWebAuthPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
